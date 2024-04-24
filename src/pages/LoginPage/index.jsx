5e3bd5c263f5255b29bdfb7deee70e8f8cfa5b4b
farhan-hafizh <farhannovyanh@gmail.com>
2024-04-24 16:01:37 +0700

    minor text fix

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -22,10 +22,9 @@ function LoginPage() {
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>
-                To keep connected with us please login with your personal info
-              </p>
+              
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
               <button
                 className="ghost"
                 id="signIn"
@@ -35,8 +34,10 @@ function LoginPage() {
               </button>
             </div>
             <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
+                <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
               <button
                 className="ghost "
                 id="signUp"
